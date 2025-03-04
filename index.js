0276e009ca111888be600d3d7a63167e09d3bebe
priyadesai99 <68284719+priyadesai99@users.noreply.github.com>
2025-03-04 11:44:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+// HINTS:
+// 1. Import express and axios
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.listen(port, ()=>{
+    console.log(`Listening on ${port}`);
+})
+
+app.get("/", async (req,res)=>{
+    try{
+        const response = await axios("https://secrets-api.appbrewery.com/random");
+        const result = response.data;
+        res.render("index.ejs", {secret: result.secret,
+            user: result.username,
+        });
+    }catch(error){
+        console.log("Failed to feth data, error: ", error);
+        res.send(500).status(error.message)
+    }
+
+})
+
+
+
+// 4. When the user goes to the home page it should render the index.ejs file.
+
+// 5. Use axios to get a random secret and pass it to index.ejs to display the
+// secret and the username of the secret.
+
+// 6. Listen on your predefined port and start the server.
